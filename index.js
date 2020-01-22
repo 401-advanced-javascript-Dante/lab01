7b605296feb9680e46296a5420ff7028bc0b98fc
dantemessy <thepunishar1@gmail.com>
2020-01-22 23:44:40 +0200

    trying to fix action

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 const validator = require('./lib/validator.js');
 
-const personRules = {
-  fields: {
-    id: {type: 'string', required: true},
-    name: {type: 'string', required: true},
-    age: {type: 'number', required: true},
-    children: { type: 'array', valueType: 'string' },
-  },
-};
-validator.isValid( personRules.fields );
+// const personRules = {
+//   fields: {
+//     id: {type: 'string', required: true},
+//     name: {type: 'string', required: true},
+//     age: {type: 'number', required: true},
+//     children: { type: 'array', valueType: 'string' },
+//   },
+// };
+validator.isValid();
 
 
 
